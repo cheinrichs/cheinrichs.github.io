f6684c734114ba8a2abce8bbccb3c2d0ea101e1a
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2020-02-17 19:46:57 -0700

    fixed animations

--- js/animations.js
+++ js/animations.js
@@ -1,49 +1,60 @@
-console.log("animations");
-
 $(document).ready( function () {
 
+  console.log("animations");
+
+
+  $('.scrollDownButton').click(function(ele){
+
+    console.log($(event.target).closest('.section').next('.section'));
+
+    $(event.target).closest('.section').next('.section').ScrollTo({
+          duration: 1500,
+          easing: 'swing'
+        })    
+  })
+  
   $('#scrollDown1').delay( 500 ).fadeIn('slow ');
 
-  $('#scrollDown1').click( function(){
-    $('#section2').ScrollTo({
-      duration: 1500,
-      easing: 'swing'
-    });
-  });
+  // $('#scrollDown1').click( function(){
+  //   $('#section2').ScrollTo({
+  //     duration: 1500,
+  //     easing: 'swing'
+  //   });
+  // });
 
-  $('#scrollDown2').click( function(){
-    $('#section3').ScrollTo({
-      duration: 1500,
-      easing: 'swing'
-    });
-  });
+  // $('#scrollDown2').click( function(){
+  //   $('#section3').ScrollTo({
+  //     duration: 1500,
+  //     easing: 'swing'
+  //   });
+  // });
 
-  $('#scrollDown3').click( function(){
-    $('#section4').ScrollTo({
-      duration: 1500,
-      easing: 'swing'
-    });
-  });
+  // $('#scrollDown3').click( function(){
+  //   $('#section4').ScrollTo({
+  //     duration: 1500,
+  //     easing: 'swing'
+  //   });
+  // });
 
-  $('#scrollDown4').click( function(){
-    $('#section5').ScrollTo({
-      duration: 1500,
-      easing: 'swing'
-    });
-  });
+  // $('#scrollDown4').click( function(){
+  //   $('#section5').ScrollTo({
+  //     duration: 1500,
+  //     easing: 'swing'
+  //   });
+  // });
 
-  $('#scrollDown5').click( function(){
-    $('#section6').ScrollTo({
-      duration: 1500,
-      easing: 'swing'
-    });
-  });
-  $('#scrollDown6').click( function(){
-    $('#section6').ScrollTo({
-      duration: 1500,
-      easing: 'swing'
-    });
-  });
+  // $('#scrollDown5').click( function(){
+  //   $('#section6').ScrollTo({
+  //     duration: 1500,
+  //     easing: 'swing'
+  //   });
+  // });
+  // $('#scrollDown6').click( function(){
+  //   $('#section6').ScrollTo({
+  //     duration: 1500,
+  //     easing: 'swing'
+  //   });
+  // });
 
 
 });
